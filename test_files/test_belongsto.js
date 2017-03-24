9fb8de9c2aa53249ed19e3577d657a0221447689
Stephen Epps <stephen.epps@mapzen.com>
2017-03-24 11:11:48 -0700

    chunked out individual tests from test files

---
+++ test_files/test_belongsto.js
@@ -0,0 +1,8 @@
+module.exports.run = function()
+{
+	successes = 0
+	failures = 0
+	console.log('Test: belongsto')
+	condition(1108730699, [102191581, 85633287, 85688877])
+	console.log('Successes:'.green, successes, 'Failures:'.red, failures)
+}
