24f3feaeb63bd2471ebdaf5d943634f6bd005009
Stephen Epps <stephen.epps@mapzen.com>
2017-06-23 11:41:31 -0700

    comments, new test files, better id testing

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var test = [
 	require('./test_runs/test_lang_names.js'),
 	require('./test_runs/test_official_lang_names.js'), //not testing currently
 	require('./test_runs/test_superseded_by.js'),
-	require('./test_runs/wk_mismatch/index.js')
+	
+	
+	//require('./test_runs/wk_mismatch/wk_mismatch_to_country.js'),
+	//require('./test_runs/wk_mismatch/wk_mismatch_to_region.js')
 ]
 
 test.forEach(function (test){
---
+++ test_runs/wk_mismatch/wk_mismatch_to_region.js
@@ -0,0 +1,93 @@
+const sink = require('through2-sink');
+const filter = require('through2-filter');
+const whosonfirst = require('pelias-whosonfirst');
+// const config = require('pelias-config').generate();
+const _ = require('lodash');
+const simplifyGeometry = require('./simplifyGeometry');
+const PolygonLookup = require('polygon-lookup');
+
+const datapath = "/usr/local/mapzen/whosonfirst-data";
+
+const placetypes = ['region', 'localadmin', 'macroregion', 'dependency',
+  'country'];
+
+const features = [];
+
+function hasWikiLatLon(properties) {
+  if (_.has(properties, 'wk:latitude') && _.has(properties, 'wk:longitude')) {
+    return true;
+  } else if (_.has(properties, 'wk:lat') && _.has(properties, 'wk:long')) {
+    return true;
+  }
+
+  return false;
+
+}
+
+function getWikiLatLon(properties) {
+  if (_.has(properties, 'wk:latitude') && _.has(properties, 'wk:longitude')) {
+    return { lat: parseFloat(properties['wk:latitude']), lon: parseFloat(properties['wk:longitude']) };
+  } else if (_.has(properties, 'wk:lat') && _.has(properties, 'wk:long')) {
+    return { lat: parseFloat(properties['wk:lat']), lon: parseFloat(properties['wk:long']) };
+  }
+}
+
+function doPlacetypes(lookup) {
+  console.log('id|placetype|lat|lon|iso country|poly country|link');
+  placetypes.forEach((placetype) => {
+    whosonfirst.metadataStream(datapath).create(placetype)
+      .pipe(whosonfirst.parseMetaFiles())
+      .pipe(whosonfirst.isNotNullIslandRelated())
+      .pipe(whosonfirst.recordHasName())
+      .pipe(whosonfirst.loadJSON(datapath, false))
+      .pipe(whosonfirst.recordHasIdAndProperties())
+      .pipe(whosonfirst.isActiveRecord())
+      .pipe(whosonfirst.conformsTo({
+        properties: (p) => {
+          return hasWikiLatLon(p) &&
+                  p.hasOwnProperty('wk:population') &&
+                  !_.has(p, 'mz:population');
+        }
+      }))
+      .pipe(filter.obj((wofData) => {
+        const wikiLatLon = getWikiLatLon(wofData.properties);
+        const poly = lookup.search( wikiLatLon.lon, wikiLatLon.lat );
+
+        if (poly && poly.properties['iso:country'] !== wofData.properties['iso:country']) {
+          const id = wofData.id;
+          const name = wofData.properties['wof:name'];
+          const poly_country = poly.properties['iso:country'];
+          const data_country = wofData.properties['iso:country'];
+
+          console.log(`${id}|${placetype}|${name}|${wikiLatLon.lat}|${wikiLatLon.lon}|${data_country}|${poly_country}|https://whosonfirst.mapzen.com/spelunker/id/${id}/`);
+
+        }
+
+      }));
+
+  });
+
+}
+
+whosonfirst.metadataStream(datapath).create('region')
+  .pipe(whosonfirst.parseMetaFiles())
+  .pipe(whosonfirst.isNotNullIslandRelated())
+  .pipe(whosonfirst.recordHasName())
+  .pipe(whosonfirst.loadJSON(datapath, false))
+  .pipe(whosonfirst.recordHasIdAndProperties())
+  .pipe(whosonfirst.isActiveRecord())
+  .pipe(simplifyGeometry.create())
+  .pipe(sink.obj((feature) => {
+    features.push(feature);
+  }))
+  .on('finish', function() {
+    doPlacetypes(new PolygonLookup( { features: features } ));
+  });
+
+//var mismatch_test = 
+//
+//module.exports.run = function()
+//{
+//	mismatch_test(getWikiLatLon)
+//	mismatch_test(doPlacetypes)
+//}
